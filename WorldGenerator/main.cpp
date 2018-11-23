099355451df0dfdc8afb7e8152d2fab4aa3efec0
COMBE Antoine <antoine.combe@edu.ece.fr>
2018-11-23 11:58:47 +0100

    premiere classe

--- WorldGenerator/main.cpp
+++ WorldGenerator/main.cpp
@@ -5,11 +5,5 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout<<"caca\n";
-    cout<<"caca\n";
-    cout<<"caca\n";
-    cout<<"caca\n";
-    cout<<"caca\n";
-
     return 0;
 }
