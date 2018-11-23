722ddd05015b11fba6d6b9426f86a72828eb92d2
COMBE Antoine <antoine.combe@edu.ece.fr>
2018-11-23 12:30:05 +0100

    yo

---
+++ WorldGenerator/include/Coords.h
@@ -0,0 +1,16 @@
+#ifndef COORDS_H
+#define COORDS_H
+
+
+class Coords
+{
+    public:
+        Coords();
+        virtual ~Coords();
+
+    protected:
+
+    private:
+};
+
+#endif // COORDS_H
---
+++ WorldGenerator/src/Coords.cpp
@@ -0,0 +1,11 @@
+#include "Coords.h"
+
+Coords::Coords()
+{
+    //ctor
+}
+
+Coords::~Coords()
+{
+    //dtor
+}
