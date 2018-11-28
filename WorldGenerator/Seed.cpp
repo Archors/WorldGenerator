98e0ebd5359f60d1970d8e6bc7274821e0e09378
COMBE Antoine <antoine.combe@edu.ece.fr>
2018-11-28 21:39:14 +0100

    Test

--- WorldGenerator/Seed.cpp
+++ WorldGenerator/Seed.cpp
@@ -1,11 +1,11 @@
 #include "Seed.h"
 
-/// Cette fonction retourne un entier aléatoire dans [min...max]
+//retourne un entier aléatoire dans [min...max]
 int alea(int min, int max, std::mt19937& randGen)
 {
     return std::uniform_int_distribution<>(min, max)(randGen);
 }
-/// Cette fonction retourne un réel aléatoire dans [min...max[
+//retourne un réel aléatoire dans [min...max[
 double alea(double min, double max, std::mt19937& randGen)
 {
     return std::uniform_real_distribution<>(min, max)(randGen);
